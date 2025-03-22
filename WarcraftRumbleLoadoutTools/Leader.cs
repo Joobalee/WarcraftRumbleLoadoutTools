773a17ea3304ac87abd769868c9e521e7aa6484f
Dale clarke <dale1901@live.com>
2025-03-22 20:28:38 +1100

    Made example more informative + fixed formatting

--- WarcraftRumbleLoadoutTools/Leader.cs
+++ WarcraftRumbleLoadoutTools/Leader.cs
@@ -19,6 +19,5 @@ namespace WarcraftRumbleLoadoutTools
             Mini = mini;
             Talent = talent;
         }
-
     }
 }
--- WarcraftRumbleLoadoutsExample/Program.cs
+++ WarcraftRumbleLoadoutsExample/Program.cs
@@ -7,27 +7,41 @@ namespace WarcraftRumbleLoadoutExample
     {
         static void Main(string[] args)
         {
-            Loadout l = Loadout.LoadoutFromCode("rumblo:CEMQABoECCYQAhoECBUQABoECF0QARoECAkQABoECB4QABoECFYQAQ==");
-                                                        //CEgQAQQIEAEIBAgQAAQIEAEECBABBAgQAA==
-            l.GetCode();
-            Console.WriteLine(JsonSerializer.Serialize(l, new JsonSerializerOptions { WriteIndented = true }));
+            // --------Import Loadout Example----------
 
-            var l2 = new Loadout(new Leader(WRMini.Sylvanas, 2));
-            l2.Troops = new List<Troop>
+            //Import the loadout from the loadout code
+            Loadout importedLoadout = Loadout.LoadoutFromCode("rumblo:CEMQABoECCYQAhoECBUQABoECF0QARoECAkQABoECB4QABoECFYQAQ==");
+            //Write the loadout as JSON to the console
+            Console.WriteLine(JsonSerializer.Serialize(importedLoadout, new JsonSerializerOptions { WriteIndented = true }));
+
+            //--------Export Loadout Example----------
+            Console.WriteLine(""); //spacing
+
+            //Create a new loadout with Sylvanas
+            var createdLoadout = new Loadout(new Leader(WRMini.Sylvanas, 2));
+            //Update the troops of the loadout
+            createdLoadout.Troops = new List<Troop>
             {
+                //Instantiate troop objects
                 new Troop(WRMini.DireBatlings, WRMiniTalent.GuanoHappens),
                 new Troop(WRMini.AngryChickens,WRMiniTalent.Snackrifice),
                 new Troop(WRMini.Ghoul,WRMiniTalent.TasteForBlood),
+                //Talents can use either WRMiniTalent enum, or a value from 0-2 to set the talent
                 new Troop(WRMini.DeepBreath,1),
                 new Troop (WRMini.ChainLightning,2),
-                new Troop(WRMini.Necromancer,1)
+                new Troop(WRMini.Necromancer,(byte)0) //0 is ambiguous between enums, ints, doubles etc so it needs to be casted as a byte in this case
             };
-            string s = l2.GetCode();
-            foreach (var t in l2.Troops)
+            Console.WriteLine(""); //spacing
+
+            //Write name of all talents to console
+            foreach (var t in createdLoadout.Troops)
             {
                 Console.WriteLine(t.Talent);
             }
-
+            Console.WriteLine(""); //spacing
+            //Write loadout code to console
+            string createdLoadoutCode = createdLoadout.GetCode();
+            Console.WriteLine(createdLoadoutCode);
         }
     }
 }
